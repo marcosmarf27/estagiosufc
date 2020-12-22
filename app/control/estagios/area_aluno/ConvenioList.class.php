9c2800e11fc14875b30eff7a3267e477a2df2b27
Marcos Casa <marcosmarf27@gmail.com>
2020-12-22 17:04:44 -0300

    melhores na interface3

--- app/control/estagios/area_aluno/ConvenioList.class.php
+++ app/control/estagios/area_aluno/ConvenioList.class.php
@@ -68,10 +68,10 @@ class ConvenioList extends TPage
         
         // creates the datagrid columns
        // $col_id    = new TDataGridColumn('id', 'Id', 'right', '5%');
-        $col_name  = new TDataGridColumn('nome', 'Razão Social', 'left', '35%');
-        $col_situacao  = new TDataGridColumn('situacao', 'Status', 'left', '15%');
-        $cnpj= new TDataGridColumn('CNPJ', 'cnpj', 'left', '15%');
-        $cpf= new TDataGridColumn('CPF', 'CPF', 'left', '15%');
+        $col_name  = new TDataGridColumn('nome', 'RAZÃO SOCIAL', 'left', '35%');
+        $col_situacao  = new TDataGridColumn('situacao', 'STATUS', 'left', '15%');
+        $cnpj= new TDataGridColumn('cnpj', 'CNPJ', 'left', '15%');
+        $cpf= new TDataGridColumn('cpf', 'CPF', 'left', '15%');
        // $tipo  = new TDataGridColumn('tipo', 'Categoria', 'left', '15%');
         $convenio = new TDataGridColumn('n_convenio', 'Nº PROCESSO', 'center', '10%');
         $validade_ini = new TDataGridColumn('validade_ini', 'INICIO VIGÊNCIA', 'center', '10%');
