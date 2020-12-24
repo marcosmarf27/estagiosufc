bc3f1a6c41676c24fb23594561975b2b6dd319c9
Marcos Casa <marcosmarf27@gmail.com>
2020-12-24 19:53:51 -0300

    melhoria parte administrativa16

--- app/control/admin/SystemAdministrationDashboard.class.php
+++ app/control/admin/SystemAdministrationDashboard.class.php
@@ -1,4 +1,12 @@
 <?php
+
+use Adianti\Control\TPage;
+use Adianti\Database\TFilter;
+use Adianti\Database\TCriteria;
+use Adianti\Database\TTransaction;
+use Adianti\Widget\Container\TVBox;
+use Adianti\Widget\Util\TXMLBreadCrumb;
+use Adianti\Widget\Template\THtmlRenderer;
 /**
  * SystemAdministrationDashboard
  *
@@ -28,16 +36,26 @@ class SystemAdministrationDashboard extends TPage
             $indicator2 = new THtmlRenderer('app/resources/info-box.html');
             $indicator3 = new THtmlRenderer('app/resources/info-box.html');
             $indicator4 = new THtmlRenderer('app/resources/info-box.html');
+            $indicator5 = new THtmlRenderer('app/resources/info-box.html');
+            $indicator6 = new THtmlRenderer('app/resources/info-box.html');
             //contar só convênios registrados em russas
             $russas = new TCriteria;
             $russas->add(new TFilter('origem',   '=',      'Russas'));
+           
+            $concluidos = new TCriteria;
+            $concluidos->add(new TFilter('situacao',   '=',      '5'));
+            
+            $ativos = new TCriteria;
+            $ativos->add(new TFilter('situacao',   '=',      '2'));
                 
             
             $indicator1->enableSection('main', ['title' => 'Estágios Avaliados',    'icon' => 'user',       'background' => 'orange', 'value' => Estagio::count()]);
             $indicator2->enableSection('main', ['title' => 'Convênios cadastrados',   'icon' => 'users',      'background' => 'blue',   'value' => Concedente::count($russas)]);
             $indicator3->enableSection('main', ['title' => 'Professores que já orientaram',    'icon' => 'university', 'background' => 'purple', 'value' => Professor::count()]);
             $indicator4->enableSection('main', ['title' => 'Alunos cadastrados', 'icon' => 'code',       'background' => 'green',  'value' => SystemUser::count()]);
-            
+            $indicator5->enableSection('main', ['title' => 'Estágios Concluidos', 'icon' => 'flag',       'background' => 'blue',  'value' => SystemUser::count($concluidos)]);
+            $indicator6->enableSection('main', ['title' => 'Estágios Ativos', 'icon' => 'code',       'background' => 'green',  'value' => SystemUser::count($ativos)]);
+
             $chart1 = new THtmlRenderer('app/resources/google_pie_chart.html');
             $data1 = [];
             $data1[] = [ 'Tipo de Estágio', 'Quantidade' ];
